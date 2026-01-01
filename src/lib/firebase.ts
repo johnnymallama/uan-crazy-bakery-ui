f7e995ccb8c5d000e634e77ecb4614f88241199c
johnnyfabianmallama <johnnyfabianmallama@gmail.com>
2026-01-01 01:37:27 +0000

    configuracion nueva authentication

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArigL1eVzC9s1wxwcHoja0GygtA8dYSJk",
-  authDomain: "crazy-bakery.firebaseapp.com",
-  projectId: "crazy-bakery",
-  storageBucket: "crazy-bakery.firebasestorage.app",
-  messagingSenderId: "327187297810",
-  appId: "1:327187297810:web:dee72d00a11758e4f60acb",
-  measurementId: "G-51XWK39Z9T"
+  apiKey: "AIzaSyAm95tZq9GX-MOg23OUo2fQrXFwhy8CFcU",
+  authDomain: "uan-especializacion.firebaseapp.com",
+  projectId: "uan-especializacion",
+  storageBucket: "uan-especializacion.firebasestorage.app",
+  messagingSenderId: "835393530868",
+  appId: "1:835393530868:web:949f2d77843860c088a7a1"
 };
 
 const app = initializeApp(firebaseConfig);
